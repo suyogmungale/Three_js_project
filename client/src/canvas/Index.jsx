44562958ff56914a63a01f090b0cadfa590b18c6
suyogmungale <suyogmungale5@gmail.com>
2023-04-02 19:10:46 +0530

    Index.jsx

---
+++ client/src/canvas/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Canvas} from '@react-three/fiber';
+import {Enviroment, Center} from '@react-three/drei';
+
+
+
+const CanvasModel = () => {
+  return (
+    <div>Canvas</div>
+  )
+}
+
+export default Canvas
